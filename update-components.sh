d02badb74492e8c4d92e10202d2c0d9381255405
Steve Wake <stephen.wake@careers.govt.nz>
2015-10-22 16:22:37 +1300

    Update components script

---
+++ update-components.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "1/4: setting up environment"
+if [ !-d vendor ]; then
+	mkdir vendor
+fi
+echo "please don't build me please SilverStripe" > vendor/_manifest_exclude
+echo "2/4: Removing current versions"
+rm -vfr vendor/leafo components/scssphp
+echo "3/4: Updating components"
+composer update --prefer-dist --no-dev
+echo "4/4: Removing cruft"
+rm -vf components/*.js
+echo "done"
