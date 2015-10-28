3eacb76f65185c2f33c178b069bcf222ab79a4f3
Steve Wake <stephen.wake@careers.govt.nz>
2015-10-29 12:15:24 +1300

    update update script to change to script directory to make automation easier

--- update-components.sh
+++ update-components.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "1/4: setting up environment"
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
+echo "In directory `pwd`"
 if [ !-d vendor ]; then
 	mkdir vendor
 fi
@@ -10,4 +13,5 @@ echo "3/4: Updating components"
 composer update --prefer-dist --no-dev
 echo "4/4: Removing cruft"
 rm -vf components/*.js components/*.css
+cd -
 echo "done"
